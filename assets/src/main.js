5689233a7306454af4ea05f5a3bd38ce07a9940b
DaikiKawaoka <kawadaiki@icloud.com>
2020-09-22 21:36:30 +0900

    環境構築 docker(go&vue)

---
+++ assets/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
+// import axios from 'axios'
+// Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
