e8987af4ed0bdf0f6c0ce4e8a0f24092b58e6f78
DaikiKawaoka <kawadaiki@icloud.com>
2020-09-22 17:34:19 +0900

    HelloWorld

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var e = createMux()
+
+func main() {
+	e.GET("/", articleIndex)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	return e
+}
+
+func articleIndex(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
