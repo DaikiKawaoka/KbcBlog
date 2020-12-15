462946f997326e591f837e3b2d7d23ccef3f611f
DaikiKawaoka <kawadaiki@icloud.com>
2020-12-15 17:51:42 +0900

    バグ修正

--- assets/babel.config.js
+++ assets/babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  pages: {
-    index: {
-      entry: 'src/main.js', // 必須パラメータ
-      title: 'ページタイトル',
-    }
-  }
+  ]
 }
