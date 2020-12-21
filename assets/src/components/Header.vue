34bfcd4b438f716ed68f10a3d13e0baa9de8c627
DaikiKawaoka <kawadaiki@icloud.com>
2020-12-21 21:40:46 +0900

    header 通知機能フロント部分作成

--- assets/src/components/Header.vue
+++ assets/src/components/Header.vue
@@ -33,16 +33,63 @@
                   <el-table-column width="100" property="name" label="name"></el-table-column>
                   <el-table-column width="300" property="address" label="address"></el-table-column>
                 </el-table> -->
-                <div>
+                <div  v-if="notifications.length > 0">
                   <p style="margin:0;"><i class="el-icon-message-solid"></i>お知らせ</p>
                   <div>
-                    <div>
-                      
+                    <div v-for="notification in notifications" :key="notification.id" class="notification-body-div">
+                      <div class="user-icon">
+                        <div class="notification-user-icon">
+                          <!-- <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag">
+                          </router-link> -->
+                        </div>
+                      </div>
+                      <div v-if="notification.action === 'alike'">
+                        <router-link class="a-tag-article" v-bind:to="{ name : 'ArticleShow', params : { id: notification.articleid.Int32 }}" >
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたの記事に「いいね！」しました。 <span class="notification-created">{{ notification.Created | moment }}</span></p>
+                        </div></router-link>
+                      </div>
+                      <div v-else-if="notification.action === 'qlike'">
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたの質問に「いいね！」しました。</p></div>
+                      </div>
+                      <div v-else-if="notification.action === 'follow'">
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたをフォローしました。</p></div>
+                      </div>
+                      <div v-else-if="notification.action === 'aclike'">
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたのコメントに「いいね！」しました。</p></div>
+                      </div>
+                      <div v-else-if="notification.action === 'qclike'">
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたのコメントに「いいね！」しました。</p></div>
+                      </div>
+                      <div v-else-if="notification.action === 'acomment'">
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたの記事に「コメント」しました。</p>
+                        </div>
+                      </div>
+                      <div v-else-if="notification.action === 'qcomment'">
+                        <div><p class="notification-text">
+                          <router-link v-bind:to="{ name : 'UserShow', params : { id: notification.userid }}" class="a-tag-n">
+                            <span class="visiter-name">{{notification.name}}</span></router-link> があなたの質問に「回答」しました。</p></div>
+                      </div>
                     </div>
                   </div>
-                  <p>通知一覧を見る</p>
+                  <router-link to="/Notifications" class="a-tag2"><p class="text-center">通知一覧を見る</p></router-link>
                 </div>
-                <el-button size="mini" slot="reference"><i class="el-icon-message-solid message-solid-i"></i></el-button>
+                <div v-else>
+                  <p style="margin:0;"><i class="el-icon-message-solid"></i>お知らせ</p>
+                  <p class="text-center">現在通知はございません。</p>
+                </div>
+                <el-button size="mini" slot="reference" @click="click_notification_btn"><i class="el-icon-message-solid message-solid-i"></i></el-button>
               </el-popover>
             </el-badge>
 
@@ -68,7 +115,7 @@
 </template>
 
 <script>
-
+import moment from 'moment'
 export default {
   props: {
     loginpage: Boolean,
@@ -79,7 +126,19 @@ export default {
   data(){
     return{
       notificationCount: localStorage.notificationCount,
+      notifications: [],
     }
+  },
+  filters: {
+    moment: function (date) {
+      moment.locale('ja')
+      return moment(date).fromNow();
+    },
+  },
+  watch: {
+    notificationCount(newNotificationCount) {
+      localStorage.notificationCount = newNotificationCount;
+    },
   },
   methods: {
     myUserPage: function() {
@@ -87,6 +146,36 @@ export default {
       if(this.$router.currentRoute.path !== `/Users/${this.user.id}`){
         this.$router.go({ name : 'UserShow', params : { id: this.user.id }})
       }
+    },
+    click_notification_btn: function(){
+    const jst = this.$cookies.get("JWT");
+    this.$axios.get('http://localhost/api/restricted/Notifications',{
+      params: {
+        // ここにクエリパラメータを指定する
+        sender: "header",
+      },
+      headers: {
+        Authorization: `Bearer ${jst}`
+      },
+    })
+      .then(response => {
+        this.notifications = response.data.Notifications
+        this.notificationCount = response.data.NotificationCount
+      })
+      .catch(error => {
+        if(error.response.status == 401){
+          if(jst !== null){
+            this.errorNotify();
+          }
+          this.$router.push({ path: "/login" });
+        }
+      })
+    },
+    errorNotify() {
+      this.$notify.error({
+        title: 'Error',
+        message: 'あなたのセッションはタイムアウトしました。再度ログインしてください。'
+      });
     },
   }
 }
@@ -174,5 +263,44 @@ export default {
 }
 .message-solid-i{
   font-size: 1.5em;
+}
+
+
+.notification-body-div{
+  display: flex;
+  border-bottom: #ccc 1px solid;
+  height: 60px;
+}
+.notification-body-div > .user-icon{
+  margin: auto 10px auto 0;
+}
+.notification-body-div > .user-icon > .notification-user-icon{
+  background-color: #ccc;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+}
+.a-tag-article{
+  text-decoration: none;
+  cursor: pointer;
+}
+.notification-text{
+  margin: 0;
+  padding: 10px;
+  color: #000;
+}
+.notification-text > .a-tag-n {
+  text-decoration: none;
+  cursor: pointer;
+}
+.visiter-name{
+  font-weight: bold;
+  color:#000;
+}
+.text-center{
+  text-align: center;
+}
+.notification-created{
+  color: #606266;
 }
 </style>
