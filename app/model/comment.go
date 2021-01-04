4a8560ea8b166ccee0340c3d780f7923e06ac72a
DaikiKawaoka <kawadaiki@icloud.com>
2021-01-04 21:48:47 +0900

    コメント整理

--- app/model/comment.go
+++ app/model/comment.go
@@ -18,25 +18,17 @@ type ArticleComment struct {
 
 // ValidationErrors ...
 func (a *ArticleComment) ValidationErrors(err error) []string {
-	// メッセージを格納するスライスを宣言します。
 	var errMessages []string
-
-	// 複数のエラーが発生する場合があるのでループ処理を行います。
 	for _, err := range err.(validator.ValidationErrors) {
-		// メッセージを格納する変数を宣言します。
 		var message string
-
-		// エラーになったフィールドを特定します。
 		switch err.Field() {
 		case "Text":
 				message = "コメントを入力してください。"
 		}
-		// メッセージをスライスに追加します。
 		if message != "" {
 			errMessages = append(errMessages, message)
 		}
 	}
-
 	return errMessages
 }
 
@@ -54,20 +46,13 @@ type QuestionComment struct {
 
 // ValidationErrors ...
 func (a *QuestionComment) ValidationErrors(err error) []string {
-	// メッセージを格納するスライスを宣言します。
 	var errMessages []string
-
-	// 複数のエラーが発生する場合があるのでループ処理を行います。
 	for _, err := range err.(validator.ValidationErrors) {
-		// メッセージを格納する変数を宣言します。
 		var message string
-
-		// エラーになったフィールドを特定します。
 		switch err.Field() {
 		case "Text":
 				message = "コメントを入力してください。"
 		}
-		// メッセージをスライスに追加します。
 		if message != "" {
 			errMessages = append(errMessages, message)
 		}
