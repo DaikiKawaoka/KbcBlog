e1411c721ff757b8a0debe8ca8eb86345104e4f8
DaikiKawaoka <kawadaiki@icloud.com>
2021-01-07 23:59:54 +0900

    画像データ返信処理追加

--- app/handler/user_handler.go
+++ app/handler/user_handler.go
@@ -1,58 +1,69 @@
 package handler
 
 import (
+	// "fmt"
 	// "log"
 	"net/http"
 	"strconv"
 	// "time"
+	// "github.com/nfnt/resize"
+	// "image"
+	_"image/gif"
+	// "image/jpeg"
+	// _"image/png"
+	// "os"
+	"encoding/base64"
+	// "encoding/json"
+	// "fmt"
+	"io/ioutil"
 
-	"app/repository"
 	"app/model"
+	"app/repository"
 	"github.com/labstack/echo/v4"
 )
 
 // UserCreateOutput ...
 type UserCreateOutput struct {
-  User          *model.User
-  Message          string
-  ValidationErrors []string
+	User             *model.User
+	Message          string
+	ValidationErrors []string
 }
 
 // UserCreate ...
 func UserCreate(c echo.Context) error {
 	var createuser model.CreateUser
 	var user model.User
-  var out UserCreateOutput
+	var out UserCreateOutput
 
-  if err := c.Bind(&createuser); err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusBadRequest, out) //400
+	if err := c.Bind(&createuser); err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusBadRequest, out) //400
 	}
 
-  if err := c.Validate(&createuser); err != nil {
-    c.Logger().Error(err.Error())
-    out.ValidationErrors = createuser.ValidationErrors(err)
-    return c.JSON(http.StatusUnprocessableEntity, out) //422
-  }
+	if err := c.Validate(&createuser); err != nil {
+		c.Logger().Error(err.Error())
+		out.ValidationErrors = createuser.ValidationErrors(err)
+		return c.JSON(http.StatusUnprocessableEntity, out) //422
+	}
 
-  res, err := repository.UserCreate(&createuser)
-  if err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError, out) //500
-  }
-  id, _ := res.LastInsertId()
-  user.SetupUser(createuser,int(id))
-  out.User = &user
-  return c.JSON(http.StatusOK,out)
+	res, err := repository.UserCreate(&createuser)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, out) //500
+	}
+	id, _ := res.LastInsertId()
+	user.SetupUser(createuser, int(id))
+	out.User = &user
+	return c.JSON(http.StatusOK, out)
 }
 
 // UserShow ...
 func UserShow(c echo.Context) error {
 	userID := userIDFromToken(c)
-	myUser,err := repository.GetMyUser(userID)
+	myUser, err := repository.GetMyUser(userID)
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"userが存在しません")
+		return c.JSON(http.StatusInternalServerError, "userが存在しません")
 	}
 
 	id, _ := strconv.Atoi(c.Param("id"))
@@ -60,125 +71,125 @@ func UserShow(c echo.Context) error {
 
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"ユーザデータを取得中にエラー発生") //500
+		return c.JSON(http.StatusInternalServerError, "ユーザデータを取得中にエラー発生") //500
 	}
 
-  if err := repository.PostCount(user); err != nil{
-    c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"ユーザの投稿数取得中にエラー発生") //500
-  }
-
-	articles, err := repository.GetUserArticle(0,user.ID)
-	if err != nil {
+	if err := repository.PostCount(user); err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"ユーザの記事一覧データを取得中にエラー発生") //500
+		return c.JSON(http.StatusInternalServerError, "ユーザの投稿数取得中にエラー発生") //500
 	}
 
-	questions, err := repository.GetUserQuestion(0,user.ID)
+	articles, err := repository.GetUserArticle(0, user.ID)
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"ユーザの質問一覧データを取得中にエラー発生") //500
+		return c.JSON(http.StatusInternalServerError, "ユーザの記事一覧データを取得中にエラー発生") //500
 	}
 
-	answerQuestions, err := repository.GetAnswerQuestionList(0,user.ID)
+	questions, err := repository.GetUserQuestion(0, user.ID)
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"ユーザの回答一覧データを取得中にエラー発生") //500
+		return c.JSON(http.StatusInternalServerError, "ユーザの質問一覧データを取得中にエラー発生") //500
+	}
+
+	answerQuestions, err := repository.GetAnswerQuestionList(0, user.ID)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "ユーザの回答一覧データを取得中にエラー発生") //500
 	}
 
 	// Follow情報を取得
 	var follow model.Follow
-	if userID != user.ID{
-		count, err := repository.CheckFollow(userID,user.ID)
+	if userID != user.ID {
+		count, err := repository.CheckFollow(userID, user.ID)
 		if err != nil {
 			c.Logger().Error(err.Error())
-			return c.JSON(http.StatusInternalServerError,"Follw情報取得中にエラー発生") //500
+			return c.JSON(http.StatusInternalServerError, "Follw情報取得中にエラー発生") //500
 		}
 		// ユーザがfollwしているか検証
-		if count > 0{
+		if count > 0 {
 			follow.IsFollow = true
 		}
 	}
 	// フォロー数取得
-	follow.FollowerCount,err = repository.GetFollowerCount(user.ID)
+	follow.FollowerCount, err = repository.GetFollowerCount(user.ID)
 	if err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError,"フォロー数取得中にエラー発生") //500
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "フォロー数取得中にエラー発生") //500
 	}
 	// フォロワー数取得
-	follow.FollowedCount,err = repository.GetFollowedCount(user.ID)
+	follow.FollowedCount, err = repository.GetFollowedCount(user.ID)
 	if err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError,"フォロワー数取得中にエラー発生") //500
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "フォロワー数取得中にエラー発生") //500
 	}
 
 	// フォローリスト取得
-	followers , err := repository.GetFollowerInfoList(user.ID)
+	followers, err := repository.GetFollowerInfoList(user.ID)
 	if err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError,"フォロワーリスト取得中にエラー発生") //500
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "フォロワーリスト取得中にエラー発生") //500
 	}
 
-  // フォロワーリスト
-	followeds , err := repository.GetFollowedInfoList(user.ID)
+	// フォロワーリスト
+	followeds, err := repository.GetFollowedInfoList(user.ID)
 	if err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError,"フォロワーリスト取得中にエラー発生") //500
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "フォロワーリスト取得中にエラー発生") //500
 	}
 
-	for _,f := range followers {
-		count, err := repository.CheckFollow(userID,f.ID);
+	for _, f := range followers {
+		count, err := repository.CheckFollow(userID, f.ID)
 		if err != nil {
 			c.Logger().Error(err.Error())
-			return c.JSON(http.StatusInternalServerError,"followcheck中にエラー発生") //500
+			return c.JSON(http.StatusInternalServerError, "followcheck中にエラー発生") //500
 		}
 		// ユーザがいいねしているか検証
-		if count > 0{
+		if count > 0 {
 			f.IsFollowing = true
-		}else{
+		} else {
 			f.IsFollowing = false
 		}
 	}
 
-	for _,f := range followeds {
-		count, err := repository.CheckFollow(userID,f.ID);
+	for _, f := range followeds {
+		count, err := repository.CheckFollow(userID, f.ID)
 		if err != nil {
 			c.Logger().Error(err.Error())
-			return c.JSON(http.StatusInternalServerError,"followcheck中にエラー発生") //500
+			return c.JSON(http.StatusInternalServerError, "followcheck中にエラー発生") //500
 		}
 		// ユーザがいいねしているか検証
-		if count > 0{
+		if count > 0 {
 			f.IsFollowing = true
-		}else{
+		} else {
 			f.IsFollowing = false
 		}
 	}
 
 	// GetArticleTagsユーザ全記事のタグ配列取得
-	tags , err := repository.GetArticleTags(user.ID)
+	tags, err := repository.GetArticleTags(user.ID)
 	if err != nil {
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError,"ユーザのタグ配列取得中にエラー発生") //500
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "ユーザのタグ配列取得中にエラー発生") //500
 	}
 
 	notificationCount, err := repository.GetNotificationCount(userID)
 	if err != nil {
 		c.Logger().Error(err.Error())
 		// クライアントにステータスコード 500 でレスポンスを返します。
-		return c.JSON(http.StatusInternalServerError,"通知数取得中にエアー発生")
+		return c.JSON(http.StatusInternalServerError, "通知数取得中にエアー発生")
 	}
 
 	// テンプレートに渡すデータを map に格納します。
 	data := map[string]interface{}{
-    "MyUser": myUser,
-    "User":user,
-		"Articles": articles,
-		"Questions": questions,
-		"AnswerQuestions" : answerQuestions,
-		"Follow": follow,
-		"Follows":followers,
-		"Followers":followeds,
-		"Tags": tags,
+		"MyUser":            myUser,
+		"User":              user,
+		"Articles":          articles,
+		"Questions":         questions,
+		"AnswerQuestions":   answerQuestions,
+		"Follow":            follow,
+		"Follows":           followers,
+		"Followers":         followeds,
+		"Tags":              tags,
 		"NotificationCount": notificationCount,
 		// "likes" : likes,
 	}
@@ -189,33 +200,33 @@ func UserShow(c echo.Context) error {
 // UserEdit ...
 func UserEdit(c echo.Context) error {
 	userID := userIDFromToken(c)
-	myUser,err := repository.GetMyUser(userID)
+	myUser, err := repository.GetMyUser(userID)
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"userが存在しません")
-  }
+		return c.JSON(http.StatusInternalServerError, "userが存在しません")
+	}
 	id, _ := strconv.Atoi(c.Param("id"))
 
 	user, err := repository.UserGetByID(id)
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"User情報取得中にエラー発生") //500
-  }
+		return c.JSON(http.StatusInternalServerError, "User情報取得中にエラー発生") //500
+	}
 
-  if myUser.ID != user.ID{
-    return c.JSON(http.StatusBadRequest,"他人のプロフィールは編集できません。")
+	if myUser.ID != user.ID {
+		return c.JSON(http.StatusBadRequest, "他人のプロフィールは編集できません。")
 	}
 
 	notificationCount, err := repository.GetNotificationCount(userID)
 	if err != nil {
 		c.Logger().Error(err.Error())
 		// クライアントにステータスコード 500 でレスポンスを返します。
-		return c.JSON(http.StatusInternalServerError,"通知数取得中にエアー発生")
+		return c.JSON(http.StatusInternalServerError, "通知数取得中にエアー発生")
 	}
 
 	data := map[string]interface{}{
-		"MyUser": myUser,
-		"User": user,
+		"MyUser":            myUser,
+		"User":              user,
 		"NotificationCount": notificationCount,
 	}
 	return c.JSON(http.StatusOK, data)
@@ -223,19 +234,19 @@ func UserEdit(c echo.Context) error {
 
 // UserUpdateOutput ...
 type UserUpdateOutput struct {
-	User         *model.User
+	User             *model.User
 	Message          string
 	ValidationErrors []string
 }
 
 // UserUpdate ...
 func UserUpdate(c echo.Context) error {
-  userID := userIDFromToken(c)
-	myUser,err := repository.GetMyUser(userID)
+	userID := userIDFromToken(c)
+	myUser, err := repository.GetMyUser(userID)
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"userが存在しません")
-  }
+		return c.JSON(http.StatusInternalServerError, "userが存在しません")
+	}
 	id, _ := strconv.Atoi(c.Param("id"))
 	if myUser.ID != id {
 		return c.JSON(http.StatusBadRequest, "他人の情報は編集できません。") //400
@@ -245,55 +256,89 @@ func UserUpdate(c echo.Context) error {
 	var out UserUpdateOutput
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, out) //400
-  }
+	}
 	// 入力値のチェック
 	if err := c.Validate(&user); err != nil {
 		out.ValidationErrors = user.ValidationErrors(err)
 		return c.JSON(http.StatusUnprocessableEntity, out)
 	}
-  user.ID = id
-  if err := repository.UserUpdate(&user); err != nil{
-    out.Message = err.Error()
-    return c.JSON(http.StatusInternalServerError, out)
-  }
+	user.ID = id
+	if err := repository.UserUpdate(&user); err != nil {
+		out.Message = err.Error()
+		return c.JSON(http.StatusInternalServerError, out)
+	}
 	out.User = &user
 	return c.JSON(http.StatusOK, out)
 }
 
+
+
+
 // UserImgUpdate ...
 func UserImgUpdate(c echo.Context) error {
-	userID := userIDFromToken(c)
-	myUser,err := repository.GetMyUser(userID)
-	if err != nil {
-		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError,"userが存在しません")
-  }
+	var imgUser model.UserImgUpdateClass
+	imgUser.ID = userIDFromToken(c)
+
 	id, _ := strconv.Atoi(c.Param("id"))
-	if myUser.ID != id {
+	if imgUser.ID != id {
 		return c.JSON(http.StatusBadRequest, "他人の情報は編集できません。") //400
 	}
 
-	if err := c.Bind(&myUser); err != nil {
-		return c.JSON(http.StatusBadRequest, "") //400
-  }
-	// 入力値のチェック
-	// if err := c.Validate(&user); err != nil {
-	// 	out.ValidationErrors = user.ValidationErrors(err)
-	// 	return c.JSON(http.StatusUnprocessableEntity, out)
-	// }
-  if err := repository.UserUpdate(myUser); err != nil{
-    c.Logger().Error(err.Error())
-    return c.JSON(http.StatusInternalServerError, "")
-  }
-	return c.JSON(http.StatusOK, "img変更成功")
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+	/*ファイルをBase64エンコード*/
+	binary, _ := ioutil.ReadAll(src)
+	imgUser.ImgData64 = base64.StdEncoding.EncodeToString(binary)
+
+	if err := repository.UserImgUpdate(&imgUser); err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "変更失敗")
+	}
+
+	user, err := repository.UserImgGetByID(imgUser.ID)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "userが存在しません")
+	}
+
+	data := map[string]interface{}{
+		"ImgData64": user.ImgData64,
+	}
+
+	return c.JSON(http.StatusOK, data)
 }
 
 // UserImgDelete ...
 func UserImgDelete(c echo.Context) error {
+	var imgUser model.UserImgUpdateClass
+	imgUser.ID = userIDFromToken(c)
+
 	id, _ := strconv.Atoi(c.Param("id"))
-	if err := repository.ArticleDelete(id); err != nil {
-		c.Logger().Error(err.Error())
-		return c.JSON(http.StatusInternalServerError, "記事削除中にエラー発生")
+	if imgUser.ID != id {
+		return c.JSON(http.StatusBadRequest, "他人の情報は編集できません。") //400
 	}
-	return c.JSON(http.StatusOK, "img削除成功")
+
+	if err := repository.UserImgDelete(&imgUser); err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "削除失敗")
+	}
+
+	user, err := repository.UserImgGetByID(imgUser.ID)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "userが存在しません")
+	}
+
+	data := map[string]interface{}{
+		"ImgData64": user.ImgData64,
+	}
+
+	return c.JSON(http.StatusOK, data)
 }
