13cd8fb5c31c02e6de1784e32a996c0e96f5c8bb
DaikiKawaoka <kawadaiki@icloud.com>
2021-01-08 17:49:06 +0900

    フォロワーとフォローリスト取得関数作成

--- app/handler/follow_handler.go
+++ app/handler/follow_handler.go
@@ -53,3 +53,87 @@ func Follow(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, "Follow 成功")
 }
+
+
+// Following ...
+func Following(c echo.Context) error {
+	userID := userIDFromToken(c)
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := repository.UserGetByID(id)
+
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "ユーザデータを取得中にエラー発生") //500
+	}
+
+	// フォローリスト取得
+	following, err := repository.GetFollowerInfoList(user.ID)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "フォローリスト取得中にエラー発生") //500
+	}
+
+
+	for _, f := range following {
+		count, err := repository.CheckFollow(userID, f.ID)
+		if err != nil {
+			c.Logger().Error(err.Error())
+			return c.JSON(http.StatusInternalServerError, "followcheck中にエラー発生") //500
+		}
+		// ユーザがいいねしているか検証
+		if count > 0 {
+			f.IsFollowing = true
+		} else {
+			f.IsFollowing = false
+		}
+	}
+
+	// テンプレートに渡すデータを map に格納します。
+	data := map[string]interface{}{
+		"Following":           following,
+	}
+
+	return c.JSON(http.StatusOK, data)
+}
+
+// Followers ...
+func Followers(c echo.Context) error {
+	userID := userIDFromToken(c)
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := repository.UserGetByID(id)
+
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "ユーザデータを取得中にエラー発生") //500
+	}
+
+	// フォロワーリスト
+	followers, err := repository.GetFollowedInfoList(user.ID)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.JSON(http.StatusInternalServerError, "フォロワーリスト取得中にエラー発生") //500
+	}
+
+
+	for _, f := range followers {
+		count, err := repository.CheckFollow(userID, f.ID)
+		if err != nil {
+			c.Logger().Error(err.Error())
+			return c.JSON(http.StatusInternalServerError, "followcheck中にエラー発生") //500
+		}
+		// ユーザがいいねしているか検証
+		if count > 0 {
+			f.IsFollowing = true
+		} else {
+			f.IsFollowing = false
+		}
+	}
+	// テンプレートに渡すデータを map に格納します。
+	data := map[string]interface{}{
+		"Followers":           followers,
+	}
+
+	return c.JSON(http.StatusOK, data)
+}
