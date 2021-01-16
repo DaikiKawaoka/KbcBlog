c48b43223f5eb1639e9e9c6b6631bb6393b8f3d1
DaikiKawaoka <kawadaiki@icloud.com>
2021-01-16 15:54:26 +0900

    1ヶ月以内でのランキングを追加

--- assets/src/components/Ranking.vue
+++ assets/src/components/Ranking.vue
@@ -1,14 +1,24 @@
 <template>
   <div v-if="isArticle" class="ranking-all">
     <!-- article -->
-    <div class="ranking-title-div" v-if="random === 0">
-      <h3 class="ranking-title">人気ユーザーランキング<i class="el-icon-user-solid"></i></h3>
+    <div class="ranking-period-all">
+      <ul class="ranking-period-ul">
+        <li v-if="rankingType === 0" class="ranking-title">
+            人気ユーザーランキング<i class="el-icon-user-solid"></i>
+        </li>
+        <li v-else class="ranking-title">
+          投稿数ランキング<i class="el-icon-document-checked"></i>
+        </li>
+        <li class="ranking-period-li" v-bind:class="{ 'now-period': nowPeriod === 'month' }" @click="getRanking('Article','month')">
+          月間
+        </li>
+        <li class="ranking-period-li" v-bind:class="{ 'now-period': nowPeriod === 'all' }" @click="getRanking('Article','all')">
+          全て
+        </li>
+      </ul>
     </div>
-    <div class="ranking-title-div post-count-ranking" v-else>
-      <h3 class="ranking-title">投稿数ランキング<i class="el-icon-document-checked"></i></h3>
-    </div>
-    <ul class="ranking-ul">
-      <li class="ranking-li" v-for="(u,index) in this.Ranking" :key="u.id" v-bind:class="{ active: user.id === u.id }">
+    <ul class="ranking-ul" v-loading="rankingLoading">
+      <li class="ranking-li" v-for="(u,index) in this.internalRanking" :key="u.id" v-bind:class="{ active: user.id === u.id }">
         <router-link v-bind:to="{ name : 'UserShow', params : { id: u.id }}" class="a-tag">
           <div class="ranking-main">
             <div class="ranking-main">
@@ -33,7 +43,7 @@
             <div class="like-count-div">
               <p class="like-count">{{ u.count }}</p>
               <p class="like-tag">
-                <i class="el-icon-star-off" v-if="random === 0"></i>
+                <i class="el-icon-star-off" v-if="rankingType === 0"></i>
                 <i class="el-icon-document" v-else></i>
               </p>
             </div>
@@ -44,14 +54,24 @@
   </div>
   <div v-else class="ranking-all">
     <!-- question -->
-    <div class="ranking-title-div" v-if="random === 0">
-      <h3 class="ranking-title">意欲的ユーザーランキング<i class="el-icon-user-solid"></i></h3>
+    <div class="ranking-period-all">
+      <ul class="ranking-period-ul">
+        <li v-if="rankingType === 0" class="ranking-title">
+            やる気ランキング<i class="el-icon-user-solid"></i>
+        </li>
+        <li v-else class="ranking-title">
+          回答数ランキング<i class="el-icon-document-checked"></i>
+        </li>
+        <li class="ranking-period-li" v-bind:class="{ 'now-period': nowPeriod === 'month' }" @click="getRanking('Question','month')">
+          月間
+        </li>
+        <li class="ranking-period-li" v-bind:class="{ 'now-period': nowPeriod === 'all' }" @click="getRanking('Question','all')">
+          全て
+        </li>
+      </ul>
     </div>
-    <div class="ranking-title-div post-count-ranking" v-else>
-      <h3 class="ranking-title">回答数ランキング<i class="el-icon-document-checked"></i></h3>
-    </div>
-    <ul class="ranking-ul">
-      <li class="ranking-li" v-for="(u,index) in this.Ranking" :key="u.id">
+    <ul class="ranking-ul" v-loading="rankingLoading">
+      <li class="ranking-li" v-for="(u,index) in this.internalRanking" :key="u.id">
         <router-link v-bind:to="{ name : 'UserShow', params : { id: u.id }}" class="a-tag">
           <div class="ranking-main">
             <div class="ranking-main">
@@ -76,7 +96,7 @@
             <div class="like-count-div">
               <p class="like-count">{{ u.count }}</p>
               <p class="like-tag">
-                <i class="el-icon-tickets" v-if="random === 0 "></i>
+                <i class="el-icon-tickets" v-if="rankingType === 0 "></i>
                 <i class="el-icon-chat-dot-round" v-else></i>
                 </p>
             </div>
@@ -91,13 +111,24 @@
   export default {
     props: {
       Ranking: Array,
-      random: Number,
+      rankingType: Number,
       isArticle: Boolean,
       user: {},
     },
     data() {
       return {
+        nowPeriod: "month",
+        internalRanking: [],
+        rankingLoading: false,
       };
+    },
+    watch: {
+      Ranking: { // 外からプロパティの中身が変更になったら実行される
+          immediate: true,
+          handler(value) {
+              this.internalRanking = value;
+          }
+      }
     },
     methods:{
       image_path(u){
@@ -112,6 +143,30 @@
           }
         }
       },
+      getRanking(type,period) {
+        this.rankingLoading = true;
+        this.$axios.get(`http://localhost/api/restricted/${type}/Ranking`, {
+        params: {
+          rankingType: this.rankingType,
+          period: period,
+        },
+        headers: {
+          Authorization: `Bearer ${this.$cookies.get("JWT")}`
+        },
+      })
+        .then(response => {
+          this.internalRanking = response.data.Ranking;
+          this.nowPeriod = period
+          this.rankingLoading = false;
+        })
+        .catch(error => {
+          if(error.response.status == 401){
+            this.$router.push({ path: "/login" });
+            this.errorNotify();
+            this.rankingLoading = false;
+          }
+        })
+      },
     }
   };
 </script>
@@ -123,9 +178,37 @@
   position: sticky;
   top: 0;
 }
+.ranking-period-all{
+  margin-top: 35px;
+  padding-top: 10px;
+  background: #fff;
+  border-radius: 5px 5px 0 0;
+}
+.ranking-period-ul{
+  display: flex;
+  margin: 0;
+  padding-left:5px;
+}
+.ranking-period-li{
+  padding:6px 6px 0 6px;
+  font-size: 0.8em;
+  color: #777;
+  cursor: pointer;
+}
+.ranking-title{
+  width: 180px;
+  padding-left: 5px;
+  font-size: 0.9em;
+  font-weight: bold;
+}
+.now-period{
+  font-weight: bold;
+  color: #333;
+}
 .ranking-ul{
   background-color: #fff;
   padding: 10px;
+  margin-top: 0px;
 }
 .ranking-main{
   display: flex;
